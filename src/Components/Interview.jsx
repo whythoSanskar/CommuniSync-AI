21c66bc97eb43d48fa1bb390a7c2861a24e73a4b
Sanskar Mishra <95489547+whythoSanskar@users.noreply.github.com>
2024-11-20 20:02:43 +0530

    Update Interview.jsx

--- src/Components/Interview.jsx
+++ src/Components/Interview.jsx
@@ -186,7 +186,7 @@ const Interview = ({ onEndInterview }) => {
     setInputMessage("");
 
     try {
-      const response = await fetch(`https://generativelanguage.googleapis.com/v1/models/gemini-1.5-pro:generateContent?key=AIzaSyDnil_v2VBSMDsgbBEd-RnZoWoOmd-bJ_M`, {
+      const response = await fetch(`https://generativelanguage.googleapis.com/v1/models/gemini-1.5-pro:generateContent?key=API_KEY`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
